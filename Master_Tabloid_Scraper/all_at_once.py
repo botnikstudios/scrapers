1d24095fbbebd8925da7464709c639e21d5ddb2d
Elle O'Brien <andronovhopf@gmail.com>
2017-08-28 11:05:45 -0700

    Some new results

--- Master_Tabloid_Scraper/all_at_once.py
+++ Master_Tabloid_Scraper/all_at_once.py
@@ -7,11 +7,53 @@ Created on Sun Aug 27 21:13:57 2017
 
 import tabloid_scrapers
 
-search_term = "Ryan Reynolds"
-days_ago = 15
+search_term = "Taylor Swift"
+days_ago = 5
 
-tabloid_scrapers.RadarScraper(search_term, days_ago)
-tabloid_scrapers.PerezScraper(search_term, days_ago)
-tabloid_scrapers.CelebuzzScraper(search_term, days_ago)
-tabloid_scrapers.PeopleScraper(search_term, days_ago)
-tabloid_scrapers.EScraper(search_term, days_ago)
+try:
+    tabloid_scrapers.RadarScraper(search_term, days_ago)
+except:
+    print('An error scraping Radar occurred.')
+
+try:
+    tabloid_scrapers.PerezScraper(search_term, days_ago)
+except:
+    print('An error scraping Perez Hilton occurred.')
+
+
+try:
+    tabloid_scrapers.CelebuzzScraper(search_term, days_ago)
+except:
+    print('An error scraping Celebuzz occurred.')
+
+
+try:
+    tabloid_scrapers.PeopleScraper(search_term, days_ago)
+except:
+    print('An error scraping People occurred.')
+
+
+try:
+    tabloid_scrapers.EScraper(search_term, days_ago)
+except:
+    print('An error scraping E online occurred.')
+
+
+try:
+    tabloid_scrapers.OKScraper(search_term, days_ago)
+except:
+    print('An error scraping OK Magazine occurred.')
+    
+
+try:
+    tabloid_scrapers.TMZScraper(search_term, days_ago)
+except:
+    print('An error scraping TMZ occurred.')
+    
+    
+
+try:
+    tabloid_scrapers.NatlEnquirerScraper(search_term, days_ago)
+except:
+    print('An error scraping The National Enquirer occurred.')
+    
--- Master_Tabloid_Scraper/tabloid_scrapers.py
+++ Master_Tabloid_Scraper/tabloid_scrapers.py
@@ -95,7 +95,8 @@ def EScraper(search_term, days_ago):
         fetched_headlines.append(headlines_list[link_indices[i]])
         
     ##################### Write to file ##########################
-    file_base = "Eonline_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "Eonline_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
     f = open(file_base + "_headlines.txt","w")
     for item in fetched_headlines:
       f.write("%s\n" % item)  
@@ -117,6 +118,7 @@ def RadarScraper(search_term, days_ago):
     import re
     import time
     from dateutil import parser
+    import os
     
     # Take out any punctuation marks from name and convert to lowercase
     search_term = search_term.translate(None, string.punctuation)
@@ -176,7 +178,8 @@ def RadarScraper(search_term, days_ago):
             fetched_headlines.append(title)
     
     # Write results to a CSV.
-    file_base = "Radar_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "Radar_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
     f = open(file_base + "_headlines.txt","w")
     for item in fetched_headlines:
       f.write("%s\n" % item)  
@@ -262,8 +265,8 @@ def PerezScraper(search_term, days_ago):
         fetched_headlines.append(headlines_list[link_indices[i]])
 
     ########### Write to file ################
-    
-    file_base = "Perez_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "Perez_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
     f = open(file_base + "_headlines.txt","w")
     for item in fetched_headlines:
       f.write("%s\n" % item)  
@@ -283,6 +286,7 @@ def PeopleScraper(search_term, days_ago):
     from datetime import datetime, timedelta
     import re
     import time
+    import os
 
     # Take out any punctuation marks from name and convert to lowercase
     search_term = search_term.translate(None, string.punctuation)
@@ -346,7 +350,8 @@ def PeopleScraper(search_term, days_ago):
             fetched_headlines.append(title_tag.encode('utf-8', errors = 'ignore'))
     
     # Write results to a CSV.
-    file_base = "People_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "People_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
     f = open(file_base + "_headlines.txt","w")
     for item in fetched_headlines:
       f.write("%s\n" % item)  
@@ -366,6 +371,7 @@ def CelebuzzScraper(search_term, days_ago):
     from datetime import datetime, timedelta
     import time
     from dateutil import parser
+    import os
     
     # Take out any punctuation marks from name and convert to lowercase
     search_term = search_term.translate(None, string.punctuation)
@@ -421,7 +427,8 @@ def CelebuzzScraper(search_term, days_ago):
         fetched_headlines.append(headlines[link_indices[i]])
 
     # Write results to a CSV.
-    file_base = "Celebuzz_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "Celebuzz_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
     f = open(file_base + "_headlines.txt","w")
     for item in fetched_headlines:
       f.write("%s\n" % item)  
@@ -432,4 +439,285 @@ def CelebuzzScraper(search_term, days_ago):
       f.write("%s\n" % item)  
     f.close()
 
+
+
+################# Nat'l Enquirer ##############################################
+def NatlEnquirerScraper(search_term, days_ago):
+    from bs4 import BeautifulSoup
+    import requests
+    import string
+    from datetime import datetime, timedelta
+    import re
+    import time 
+    import os
     
+    # Take out any punctuation marks from name and convert to lowercase
+    search_term = search_term.translate(None, string.punctuation)
+    search_term = search_term.lower()# 
+    # Format search term for the URL formula
+    search_term_formatted = search_term.replace(" ", "+")
+    
+    page_link = "http://www.nationalenquirer.com/search/?search=" + search_term_formatted
+    page = requests.get(page_link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    
+    articles = soup.find_all(class_="post-detail")
+    timestamp = []
+    article_link = []
+    fetched_stories = []
+    fetched_headlines = []
+    fetched_subheads = []
+    
+    
+    # For every article on the page, get the link and the time it was posted at.
+    for i in range(0,len(articles)):
+        article_tags = articles[i]
+    
+        
+        time_children = article_tags.find_all(class_="posted-date")
+        if not time_children:
+            timestamp.append(datetime.strptime('January 1 1900', "%B %d %Y"))
+        else:
+            timestamp_tmp = time_children[0].get_text()
+            timestamp_stripped = re.findall('Posted (.*?) @', timestamp_tmp)
+            timestamp_stripped = timestamp_stripped[0]
+    
+            timestamp_stripped = timestamp_stripped.encode('utf-8', errors='ignore')
+            timestamp_stripped = timestamp_stripped.translate(None, string.punctuation)
+            timestamp.append(datetime.strptime(timestamp_stripped, "%b %d %Y"))
+        
+        article_link_children = article_tags.find_all('a')
+        article_link_tmp = article_link_children[0]
+        article_link.append(article_link_tmp['href'].encode('utf-8',errors='ignore'))
+    
+    # Which results are recent?
+    is_recent = [datetime.today() - timedelta(days=days_ago) < x for x in timestamp]
+    link_indices = [i for i, x in enumerate(is_recent) if x]
+    
+    # Loop over the recent results- DUMB. The national enquirer actually only has 
+    # headlines, no articles. All the articles are in the form of galleries. Sigh.
+    for i in range(0,len(link_indices)):
+        page_result_link = article_link[i]
+        page_result = requests.get(page_result_link)
+        res_soup = BeautifulSoup(page_result.content, 'html.parser')
+        story_tag = res_soup.find_all(type="application/ld+json")
+        if len(story_tag) > 1:
+            story_tmp = story_tag[1].get_text()
+            story = re.findall('caption":"(.*?)","url"', story_tmp)
+            story = ' '.join(story)
+            story = story.decode('unicode_escape')
+            fetched_stories.append(story.encode('utf-8',errors='ignore'))
+            title_tag = res_soup.find_all("title")
+            title = title_tag[0].get_text()
+            subtitle_tag = res_soup.find_all(class_="entry-subtitle")
+            subtitle = subtitle_tag[0].get_text()
+            fetched_headlines.append(title.encode('utf-8', errors = 'ignore'))
+            fetched_subheads.append(subtitle.encode('utf-8', errors = 'ignore'))
+        
+        
+    # Write results to a CSV.
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "NatlEnq_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    f = open(file_base + "_headlines.txt","w")
+    for item in fetched_headlines:
+      f.write("%s\n" % item)  
+    f.close()
+    
+    f = open(file_base + "_articles.txt","w")
+    for item in fetched_stories:
+      f.write("%s\n" % item)  
+    f.close()
+
+
+############# TMZ #############################################################
+def TMZScraper(search_term, days_ago):
+    from bs4 import BeautifulSoup
+    import requests
+    import string
+    import re
+    from selenium import webdriver
+    from dateutil import parser
+    from datetime import datetime, timedelta
+    import time
+    from HTMLParser import HTMLParser 
+    import os
+    from sys import platform
+    
+    h = HTMLParser()
+
+    # Take out any punctuation marks from name and convert to lowercase
+    search_term = search_term.translate(None, string.punctuation)
+    search_term = search_term.lower()
+    
+    # Format search term for the URL formula
+    search_term_formatted = search_term.replace(" ", "+")
+
+    
+    page_link = "http://tmz.com/search/news/" + search_term_formatted +"?adid=TMZ_Web_Nav_Search"
+    domain = "http://www.tmz.com"
+    
+    
+    if platform == "linux" or platform == "linux2":
+        cwd = os.getcwd()
+        browser = webdriver.Chrome(cwd + '/chromedriver')
+    elif platform == "win32" or platform == "win64":
+        browser = webdriver.Chrome()
+    browser.get(page_link)
+    html = browser.page_source
+    browser.close()
+    browser.quit()
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    articles = soup.find_all(class_ = "title") # This will get some junk, too. 
+    
+    
+    #### Initialize the vectors for storing results
+    timestamp= []
+    article_link_list = []
+    fetched_stories = []
+    fetched_headlines=[]
+    
+    for i in range(1, len(articles)):
+        child = articles[i].find_all(class_="has-adid")
+        
+        # If it really is a search result- as in, child isn't empty-
+        if child:
+            headline = child[0].get_text()
+            link = child[0]['href']
+            article_link_list.append(link.encode('utf-8', errors='ignore'))
+            date = link[1:12].encode('utf-8', errors='ignore') # TMZ encodes date strings in first 12 char of URL
+            timestamp.append(parser.parse(date))
+    
+    ##### Of the results we got, how many are recent enough?
+    is_recent = [datetime.today() - timedelta(days=days_ago) < x for x in timestamp]
+    link_indices = [i for i, x in enumerate(is_recent) if x]
+    
+    # Loop over the recent results
+    for i in range(0,len(link_indices)):
+        page_result_link = domain + article_link_list[link_indices[i]]
+        page_result = requests.get(page_result_link)
+        res_soup = BeautifulSoup(page_result.content, 'html.parser')
+        story_tag = res_soup.find_all(type="application/ld+json")
+        story_tmp = story_tag[0].get_text()
+        story = re.findall('articleBody": "(.*?)"\n}', story_tmp)
+        
+        # Clean up the text- TMZ litters with HTML string
+        story = story[0].encode('utf-8').replace('\\n', ' ')
+        story = h.unescape(story)
+        story = story.encode('utf-8').replace('\\', ' ')
+        
+        
+        headline = re.findall('headline": "(.*?)",', story_tmp)
+        headline = h.unescape(headline[0])
+        headline = headline.encode('utf-8')
+    
+        
+        fetched_stories.append(story)
+        fetched_headlines.append(headline)
+    ################## Write to file ##########################
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "TMZ_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    f = open(file_base + "_headlines.txt","w")
+    for item in fetched_headlines:
+      f.write("%s\n" % item)  
+    f.close()
+    
+    f = open(file_base + "_articles.txt","w")
+    for item in fetched_stories:
+      f.write("%s\n" % item)  
+    f.close()
+    
+    
+    
+######################## OK Magazine ##########################################
+def OKScraper(search_term, days_ago):    
+    from bs4 import BeautifulSoup
+    import requests
+    import string
+    from datetime import datetime, timedelta
+    import time
+    from dateutil import parser
+    import os
+    
+
+    # Take out any punctuation marks from name and convert to lowercase
+    search_term = search_term.translate(None, string.punctuation)
+    search_term = search_term.lower()
+    
+
+    # Format search term for the URL formula
+    search_term_formatted = search_term.replace(" ", "+")
+
+    page_link = "http://okmagazine.com/search/?search=" + search_term_formatted
+    page = requests.get(page_link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    timestamp = []
+    article_link_list = []
+    headlines = []
+    fetched_stories = []
+    fetched_headlines = []
+    
+    
+    articles = soup.find_all("article")
+    
+    # OK magazine doesn't give dates- it just lists in chronological order a lot of results.
+    # So, grab maybe 4, and we'll check if each one is within the date range. 
+    for i in range(0, min(len(articles),4)):
+        this_article = articles[i]
+        
+        headline_tag = this_article.find_all(class_="promo-title")
+        link_tag = this_article.find_all("a")
+        
+        # Update headline list 
+        headline = headline_tag[0].get_text()
+        headlines.append(headline.encode('utf-8'))
+        
+        # Get the link
+        link = link_tag[0]['href']
+        article_link_list.append(link.encode('utf-8'))
+     
+    # NEXT PART: Figure out which dates are within a window    
+    for i in range(0,len(headlines)):
+        page_result_link = article_link_list[i]
+        page_result = requests.get(page_result_link)
+        res_soup = BeautifulSoup(page_result.content, 'html.parser')
+        story_tag = res_soup.find_all(class_="gallery-item-caption")
+        story = ""
+        for j in range(0,len(story_tag)):
+            story_tmp = story_tag[j].get_text()
+            story_tmp = story_tmp.encode('utf-8')
+            story += story_tmp
+    
+        # Also get the date it occurred on
+        date_tag = res_soup.find_all(class_="posted-date")
+        date_str = date_tag[0].get_text()
+        date_str = date_str.encode('utf-8').strip()
+        
+        # Append to master lists
+        fetched_stories.append(story)
+        fetched_headlines.append(headlines[i])
+        timestamp.append(parser.parse(date_str[0:15]))
+        
+        
+      # Which articles were posted within N days?    
+    is_recent = [datetime.today() - timedelta(days=days_ago) < x for x in timestamp]
+    link_indices = [i for i, x in enumerate(is_recent) if x]
+        
+    
+    # Write results to a CSV.
+    results_loc = os.getcwd() + "/Results/"
+    file_base = results_loc + "OKMagazine_" + search_term.lower().replace(" ", "_") + "_" + time.strftime("%d_%m_%y")
+    f = open(file_base + "_headlines.txt","w")
+    for item in link_indices:
+      f.write("%s\n" % fetched_headlines[item])  
+    f.close()
+    
+    f = open(file_base + "_articles.txt","w")
+    for item in link_indices:
+      f.write("%s\n" % fetched_stories[item])  
+    f.close()
+
+    
+
